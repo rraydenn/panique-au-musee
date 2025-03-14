b0585fd9ae757528132787a9eae55cf0e8aa976b
POUPON UGO p2020982 <ugo.poupon@etu.univ-lyon1.fr>
2025-03-14 18:18:25 +0100

    TP1 - Partie 6-2.5: ajout commentaire (checkstyle)

--- users/src/main/java/fr/univlyon1/m1if/m1if13/users/config/WebMvcConfig.java
+++ users/src/main/java/fr/univlyon1/m1if/m1if13/users/config/WebMvcConfig.java
@@ -2,7 +2,7 @@ package fr.univlyon1.m1if.m1if13.users.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
+//import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
